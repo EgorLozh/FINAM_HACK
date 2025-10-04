232abb357c6f21b8d1a1c0238169c56219c1970a
EgorLozh <lozhkinyegor@yandex.ru>
2025-10-04 20:20:57 +0400

    Repo for clickhouse

--- app_analytics/core/config/base.py
+++ app_analytics/core/config/base.py
@@ -33,6 +33,14 @@ class Settings(BaseSettings, BaseSingleton["Settings"]):
     OPEN_ROUTER_MODEL: str
     OPEN_ROUTER_URL: str
 
+    # Clickhouse
+    CLICKHOUSE_HOST: str
+    CLICKHOUSE_PORT: int = 8123
+    CLICKHOUSE_DB: str
+    CLICKHOUSE_USER: str
+    CLICKHOUSE_PASSWORD: str
+    CLICKHOUSE_DEFAULT_ACCESS_MANAGEMENT: int = 1
+
     class Settings(BaseSettings):
         model_config = SettingsConfigDict(env_file='.env', extra='ignore')
     
@@ -52,6 +60,15 @@ class Settings(BaseSettings, BaseSingleton["Settings"]):
             f"postgresql+psycopg2://{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}"
             f"@{self.POSTGRES_HOST}:{self.POSTGRES_PORT}/{self.POSTGRES_DB}"
         )
+    
+    @computed_field
+    @property
+    def CLICKHOUSE_URL(self) -> str:
+        return (
+            f"clickhouse+http://{self.CLICKHOUSE_USER}:{self.CLICKHOUSE_PASSWORD}"
+            f"@{self.CLICKHOUSE_HOST}:{self.CLICKHOUSE_PORT}/{self.CLICKHOUSE_DB}"
+        )
+
 
     @computed_field
     @property
